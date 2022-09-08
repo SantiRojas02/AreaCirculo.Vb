b305baf44eb56fc5538260aa4d3bcbe66e349084
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-07 22:04:06 -0300

    AreaCirculo

---
+++ Area_de_circulo.vb/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Dim radio, area As String
+        Console.Write("Ingrese el radio del ciruculo: ")
+        radio = Console.ReadLine()
+
+
+        area = 3.1415 * radio * radio
+
+        Console.WriteLine("El area del circulo es: " & area)
+
+        Console.ReadKey()
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
